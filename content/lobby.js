cf6627e4b6037a4fc61a9b691606a11dbb29579a
Ivar Hammarberg <ivar.hammarberg@gmail.com>
2022-09-25 15:43:00 +0200

    Started lobby client side

---
+++ content/lobby.js
@@ -0,0 +1,34 @@
+const allowedIds = {"T8":"Vändåtta"};
+
+const init = () =>
+{
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('game');
+
+    if (allowedIds[id]!=undefined)
+    {
+        document.querySelector('#main-title').innerText =allowedIds[id]
+        let wsckt = new WebSocket("ws://" + window.location.href.split('//')[1].split('?')[0]);
+        wsckt.addEventListener('open', () =>
+        {
+            wsckt.send(JSON.stringify({ "gameId": id, "name": "steve" }));
+            wsckt.addEventListener('message', (e) =>
+            {
+                const msg = e.data;
+            });
+
+
+
+
+        });
+    }
+    else 
+    {
+        setTimeout(() =>
+        {
+            window.location.href = "/";
+        }, 5000);
+    }
+};
+
+init();
