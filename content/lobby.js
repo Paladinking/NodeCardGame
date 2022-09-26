2ebd84c8d3cbbebf5ef00fa207bdacba77dd0975
Ivar Hammarberg <ivar.hammarberg@gmail.com>
2022-09-26 18:18:00 +0200

    Updated lobby appearance

--- content/lobby.js
+++ content/lobby.js
@@ -1,13 +1,19 @@
-const allowedIds = { "T8": "Vändåtta", "CN": "Caravan" };
+const GAME_TYPES = { "T8": { name: "Vändåtta", minPlayers: 2 }, "CN": { name: "Caravan", minPlayers: 2, maxPlayers: 2 } };
+const CARD_NUMBERS = "A23456789JQK";
+const CARD_COLORS = "SCDH";
+const gameId = new URLSearchParams(window.location.search).get('game');
+const playersSpan = document.querySelector('#players');
+
 
 const init = (name) =>
 {
     const ul = document.querySelector('#playlist');
+    let playerCount = 0;
     ul.innerHTML = "";
     let wsckt = new WebSocket("ws://" + window.location.href.split('//')[1].split('?')[0]);
     wsckt.addEventListener('open', () =>
     {
-        wsckt.send(JSON.stringify({ "gameId": id, "name": name }));
+        wsckt.send(JSON.stringify({ "gameId": gameId, "name": name }));
         wsckt.addEventListener('message', (e) =>
         {
             const msg = JSON.parse(e.data);
@@ -19,11 +25,14 @@ const init = (name) =>
                     const li = document.createElement('li');
                     li.innerText = player;
                     ul.append(li);
+                    playerCount++;
                 }
                 const li = document.createElement('li');
                 li.classList.add('new');
                 li.innerText = name;
                 ul.append(li);
+                playerCount++;
+                playersSpan.innerText = `${playerCount}/${GAME_TYPES[gameId].minPlayers}`;
             }
             if (msg.event == 'join')
             {
@@ -33,20 +42,22 @@ const init = (name) =>
                 ul.append(li);
             }
         });
+        window.addEventListener('beforeunload', (e) =>
+        {
+            e.preventDefault();
+            return e.returnValue = "Are you sure you want to exit?";
+
+        });
     });
 
 };
 
-
-const params = new URLSearchParams(window.location.search);
-const id = params.get('game');
-
-if (allowedIds[id] != undefined)
+if (GAME_TYPES[gameId] != undefined)
 {
-    document.querySelector('#main-title').innerText = allowedIds[id];
+    document.querySelector('#main-title').innerText = GAME_TYPES[gameId].name;
+    playersSpan.innerText = `0/${GAME_TYPES[gameId].minPlayers}`;
     document.querySelector('#join-button').addEventListener('click', () =>
     {
-
         const name = document.querySelector('#join-input').value;
         if (name)
         {
@@ -61,3 +72,43 @@ else
         window.location.href = "/";
     }, 5000);
 }
+
+let startBackgroundCards = () =>
+{
+    const main = document.querySelector('#background-wrapper');
+    for (let i = 0; i < 15; i++)
+    {
+        const number = CARD_NUMBERS[Math.floor(Math.random() * CARD_NUMBERS.length)];
+        const color = CARD_COLORS[Math.floor(Math.random() * CARD_COLORS.length)];
+        const card = document.createElement('div');
+        card.classList.add('background-card-wrapper');
+        card.innerHTML = `<img src = "./2color/${number}${color}.svg" draggable = "false" width = 140>`;
+        main.prepend(card);
+        const animate = () =>
+        {
+            const timing = 2000 + Math.floor(Math.random() * 10000);
+            const x = Math.floor(Math.random() * main.offsetWidth + 200) - 400;
+            const y = -200;
+            const xOffset = (50 + Math.floor(Math.random() * 100)) * (Math.round(Math.random()) == 0 ? -1 : 1);
+            const yOffset = (main.offsetHeight + 200);
+            const rotation = Math.floor(Math.random() * 180) * (Math.round(Math.random()) == 0 ? -1 : 1);
+            card.style.transform = `rotate(${rotation}deg)`;
+            card.animate(
+                [{
+                    left: `${x}px`,
+                    top: `${y}px`
+                },
+                {
+                    left: `${x + xOffset}px`,
+                    top: `${y + yOffset}px`
+
+                }], timing);
+            setTimeout((animate), timing);
+
+        };
+        animate();
+
+    }
+};
+
+startBackgroundCards();
