2bf65f2555242917b7acdaff56b3e042b39405bf
Axel <axel.hammarberg@gmail.com>
2022-09-26 19:01:50 +0200

    Handle close + most of start

--- server.js
+++ server.js
@@ -64,25 +64,25 @@ const sendError = (res, code, msg = errors[code]) => {
 };
 
 const handleGet = (req, res) => {
-	const obj = findFile(req.url);
-	if (obj.status == 200) {
-		if (obj.plain) {
-			fs.readFile("./content" + obj.url, "utf8", (err, data) => {
+	const resObj = findFile(req.url);
+	if (resObj.status == 200) {
+		if (resObj.plain) {
+			fs.readFile("./content" + resObj.url, "utf8", (err, data) => {
 				if (err) {
 					sendError(res, 500);
 				} else {
-					res.writeHead(200, {'Content-Type' : obj.contentType});
+					res.writeHead(200, {'Content-Type' : resObj.contentType});
 					res.write(data);
 					res.end();
 				}
 			});
 		} else {
-			fs.createReadStream("./content" + obj.url).pipe(res);
+			fs.createReadStream("./content" + resObj.url).pipe(res);
 		}
-	} else if (obj.status == 301) {
-		res.writeHead(301, {location : obj.url});
+	} else if (resObj.status == 301) {
+		res.writeHead(301, {location : resObj.url});
 		res.end();
-	} else if (obj.status == 404) {
+	} else if (resObj.status == 404) {
 		sendError(res, 404);
 	} else {
 		sendError(res, 500, "Unreachable");
@@ -96,17 +96,6 @@ const isValidMsg = (msg, requiredKeys) => {
 	return true;
 };
 
-const kickLobbyPlayer = (socket, reason) => {
-	socket.close(1000, reason);
-	socket.game.lobby.players.splice(socket.game.id, 1);
-	for (let i = 0; i < socket.game.lobby.players.length; i++) {
-		socket.game.lobby.players[i].send(`{"event" : "leave", "id" : ${socket.game.id}}`);
-		if (socket.game.lobby.players[i].game.id != i) {
-			socket.game.lobby.players[i].game.id = i;
-		}
-	}
-};
-
 const server = http.createServer((req, res) => {
 	console.log(req.url);
 	if (req.method == "GET") {
@@ -117,8 +106,7 @@ const server = http.createServer((req, res) => {
 });
 
 /*
-
-	//lobby object
+	//lobby object -- one per game type
 	game[game] = {
 		minPlayers : minimum number of players
 		maxPlayers : maximum number of players
@@ -130,6 +118,13 @@ const server = http.createServer((req, res) => {
 		id : index in lobby.players
 		name : name string,
 		lobby : lobby object for relevant game
+		
+	//game object -- many per game (in theory)
+	{
+		players : list of sockets,
+		handle_message : function(socket.game, data), 
+		..game data unique to each game
+	}
  }
  */
 server.listen(port, hostname, () => {
@@ -140,6 +135,7 @@ const socketServer = new ws.WebSocketServer({server : server});
 
 const games = {
 	"T8" : {
+		gameName : "T8",
 		minPlayers : 2,
 		maxPlayers : 5,
 		players : []
@@ -180,27 +176,46 @@ socketServer.on("connection", (socket) => {
 				socket.game.status = IN_LOBBY;
 				socket.game.lobby = lobby;
 
+				const names = lobby.players.map(s => s.game.name);
 				for (let i = 0; i < lobby.players.length; i++) {
 					lobby.players[i].send(`{"event" : "join", "name" : "${socket.game.name}", "id" : ${socket.game.id}}`);
 				}
-				socket.send(`{"event" : "joined", "players" : [${lobby.players}], "minPlayers" : ${lobby.minPlayers}, "maxPlayers" : ${lobby.maxPlayers}}`);
+				socket.send(`{"event" : "joined", "players" : [${names}]}`);
 				lobby.players.push(socket);
 				break;
 			case IN_LOBBY:
 				if (!isValidMsg(data, ["action"])) {
-					kickLobbyPlayer(socket, "Invalid message");
+					socket.close(1000, "Invalid message");
 					return;
 				}
 				if (data.action == "Leave") {
-					kickLobbyPlayer(socket, "Left game");
+					socket.close(1000, "Left game");
 					return;
 				}
-
+				if (data.action == "Start") {
+					if (socket.game.lobby.players.length >= socket.game.lobby.minPlayers) {
+						for (let sock of socket.game.lobby) {
+							sock.send(`{"event" : "start}"`);
+						}
+					} else {
+						socket.close(1000, "Not enough players");
+					}
+				}
+				socket.close(1000, "Invalid message");
 				break;
 		}
 
 	});
 	socket.on("close", (code, reason) => {
-
+		if (socket.game.status == IN_LOBBY) {
+			socket.game.lobby.players.splice(socket.game.id, 1);
+			for (let i = 0; i < socket.game.lobby.players.length; i++) {
+				socket.game.lobby.players[i].send(`{"event" : "leave", "id" : ${socket.game.id}}`);
+				if (socket.game.lobby.players[i].game.id != i) {
+					socket.game.lobby.players[i].game.id = i;
+				}
+			}
+		}
+		console.log(socket.game.name + " left");
 	});
 });
